62d26986a586e37286d3bb252301097e1a3830d8
Jere Lavikainen <61044352+jerela@users.noreply.github.com>
2026-04-20 18:25:25 +0300

    Update utils_test.py

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,6 +1,5 @@
 from mola import Matrix
 from mola import utils
-import statistics
 
 def test_matrix_variance():
     """Test the computation of the variance of a matrix."""
@@ -8,4 +7,4 @@ def test_matrix_variance():
     x_col = Matrix([[1], [2], [4], [7]])
     var_row = utils.var(x_row)
     var_col = utils.var(x_col)
-    assert(abs(var_row-statistics.variance(x_row)) < 1e-12 and abs(var_col-statistics.variance(x_col)) < 1e-12)
+    assert(abs(var_row-7) < 1e-12 and abs(var_col-7) < 1e-12)
